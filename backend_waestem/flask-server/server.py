3437fc70472ac4f3713a8697fae4f1e6ff23ff2f
JuntaoLi79 <61711097+shane1595042264@users.noreply.github.com>
2023-03-28 08:16:24 -0400

    Update server.py

--- backend_waestem/flask-server/server.py
+++ backend_waestem/flask-server/server.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_HOST'] = 'mysql'
 app.config['MYSQL_USER'] = 'u408394733_double'
-app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_PASSWORD'] = 'Mark321654.'
 app.config['MYSQL_DB'] = 'u408394733_waestem'
 
 mysql = MySQL(app)
