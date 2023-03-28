f6542947333ef13b13ed5ad546946e1a9a9b28b2
JuntaoLi79 <61711097+shane1595042264@users.noreply.github.com>
2023-03-28 08:05:23 -0400

    Update server.py

--- backend_waestem/flask-server/server.py
+++ backend_waestem/flask-server/server.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = ''
+app.config['MYSQL_USER'] = 'u408394733_double'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'u408394733_waestem'
 
+mysql = MySQL(app)
 
-@app.route('/members')
-# Sample member skeleton
-def members():
-    return {"members": ["John", "Paul", "George", "Ringo"]}
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT * FROM mytable''')
+    rv = cur.fetchall()
+    return str(rv)
 
 if __name__ == '__main__':
     app.run(debug=True)
