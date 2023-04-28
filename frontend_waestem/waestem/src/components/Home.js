93de980daaf0e817b420a20e791244b8c74caa9d
jepetro <97709211+jepetro@users.noreply.github.com>
2023-04-28 19:41:30 -0400

    refactor: added back buttons

--- frontend_waestem/waestem/src/components/Home.js
+++ frontend_waestem/waestem/src/components/Home.js
@@ -28,8 +28,14 @@ const Home = () => {
       <div className="absolute inset-0 flex flex-col justify-center items-center">
         <img src={logo} alt="logo" border="0" className="w-1/4" />
         <h1 className="text-5xl font-bold text-white mb-10">Welcome to Waestem!</h1>
-        <div></div>
-        <p className='text-white'>Waestem is a revolutionary travel social media website that allows you to connect with fellow travelers from across the world. Join us by creating an account today to show us and the world the travels you've been on.</p>
+        <div className="flex space-x-4">
+          <button className="bg-white text-black py-3 px-6 rounded-lg font-bold shadow-lg" onClick={handleViewPostsClick}>
+            View Posts
+          </button>
+          <button className="bg-white text-black py-3 px-6 rounded-lg font-bold shadow-lg" onClick={handleCreatePostClick}>
+            Create Post
+          </button>
+        </div>
       </div>
     </div>
   );
